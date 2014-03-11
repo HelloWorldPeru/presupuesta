b28e534c7cdf7840060ee67b1b12830337f9e4e1
jonathancg90 <jonathancg90@gmail.com>
2014-03-10 23:09:25 -0500

    project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Presupuesta::Application.config.secret_key_base = '8e364679befa1d1511caaa9b1dda6e9da757c10ab8aed230ad13019c3ec6c3e56539d7e86258c26d19e326645d490ba8e3540896548cc6ea473b8ced631a4004'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Presupuesta::Application.config.session_store :cookie_store, key: '_presupuesta_session'
